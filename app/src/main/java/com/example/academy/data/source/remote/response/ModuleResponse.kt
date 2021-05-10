bcdc3478bbc136643090e2503505318f20d133e8
petersam <man.sanji23@gmail.com>
2021-05-10 22:19:34 +0700

    idling resource

--- app/src/main/java/com/example/academy/data/source/remote/response/ModuleResponse.kt
+++ app/src/main/java/com/example/academy/data/source/remote/response/ModuleResponse.kt
@@ -1,7 +1,7 @@
 package com.example.academy.data.source.remote.response
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class ModuleResponse(
